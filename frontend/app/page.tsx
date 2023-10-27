8ddbda6cde128eac37bd7d31e1312281f453fd05
blackneb <antenehcs@gmail.com>
2023-10-27 13:28:09 +0300

    create account

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import Navbar from './widgets/Navbar'
 import LoginPage from './components/LoginPage'
+import CreateAccount from './components/CreateAccount'
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
-      <LoginPage/>
+      <CreateAccount/>
     </div>
   )
 }
