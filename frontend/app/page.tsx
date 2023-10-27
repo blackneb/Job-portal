fb18b5e7cf9687fff8833321bf80e3c3d0038218
blackneb <antenehcs@gmail.com>
2023-10-27 13:30:55 +0300

    profile

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,12 +2,13 @@ import Image from 'next/image'
 import Navbar from './widgets/Navbar'
 import LoginPage from './components/LoginPage'
 import CreateAccount from './components/CreateAccount'
+import Profile from './components/Profile'
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
-      <CreateAccount/>
+      <Profile/>
     </div>
   )
 }
