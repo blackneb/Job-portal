225b94ca739efc7b9d23d9f0394e940cb347db6f
blackneb <antenehcs@gmail.com>
2023-10-27 13:53:16 +0300

    post new Job

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,12 +3,13 @@ import Navbar from './widgets/Navbar'
 import LoginPage from './components/LoginPage'
 import CreateAccount from './components/CreateAccount'
 import Profile from './components/Profile'
+import PostAJob from './components/PostAJob'
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
-      <Profile/>
+      <PostAJob/>
     </div>
   )
 }
