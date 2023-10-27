8b2e0f187682fa892094d2089f5e6d0393f81b8a
blackneb <antenehcs@gmail.com>
2023-10-27 13:57:55 +0300

    upload resume

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,12 +4,14 @@ import LoginPage from './components/LoginPage'
 import CreateAccount from './components/CreateAccount'
 import Profile from './components/Profile'
 import PostAJob from './components/PostAJob'
+import UploadResume from './components/UploadResume'
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
-      <PostAJob/>
+      <UploadResume/>
+
     </div>
   )
 }
