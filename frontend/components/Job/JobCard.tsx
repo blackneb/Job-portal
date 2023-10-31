a03099f5a8e99d4afea3ad79560eaaefbf1b4e6c
blackneb <antenehcs@gmail.com>
2023-10-31 17:17:19 +0300

    job card

--- frontend/components/Job/JobCard.tsx
+++ frontend/components/Job/JobCard.tsx
@@ -1,24 +1,39 @@
 import React from 'react'
+import Image from 'next/image'
 
 const JobCard = ({ companyName, jobTitle, description, industry, type, salary, expireDate }:any) => {
   return (
     <div className="w-[48rem] rounded overflow-hidden shadow-lg bg-white m-4">
       <div className="px-6 py-4">
-        <div className="mb-2">{companyName}</div>
-        <p className="text-gray-700 text-base">{jobTitle}</p>
+        <div className="mb-2 flex flex-row">
+            <Image src="/company.png" alt="Icon" width={20} height={10} />
+            <p className='ml-2 text-gray-500'>{companyName}</p>
+        </div>
+        <div className="mb-2 flex flex-row">
+            <Image src="/tag.png" alt="Icon" width={20} height={10} />
+            <p className='ml-2'>{jobTitle}</p>
+        </div>
+        <div className="mb-2 flex flex-row">
+            <p className='ml-2 text-xs'>{description.slice(0,240)}...</p>
+        </div>
       </div>
-      <div className="px-6 py-4 mb-8">
-        <p className="text-gray-700 text-base">{description}</p>
-      </div>
-      <div className="px-6 pb-4 flex bg-gray-800 justify-between items-center">
-        <p className="text-white mx-4">{industry}</p>
-        <p className="text-white mx-4">{type}</p>
-        <p className="text-white text-sm m-4">
-          Salary: {salary}
-        </p>
-        <p className="text-red-500 text-sm">
-          Expires on: {expireDate}
-        </p>
+      <div className="px-6 py-2 flex bg-gray-100 justify-between items-center">
+        <div className="mb-2 flex flex-row">
+            <Image src="/industry.png" alt="Icon" width={20} height={10} />
+            <p className='ml-2 text-sm'>{industry}</p>
+        </div>
+        <div className="mb-2 flex flex-row">
+            <Image src="/portfolio.png" alt="Icon" width={20} height={10} />
+            <p className='ml-2 text-sm'>{type}</p>
+        </div>
+        <div className="mb-2 flex flex-row">
+            <Image src="/salary.png" alt="Icon" width={20} height={10} />
+            <p className='ml-2 text-sm'>{salary}</p>
+        </div>
+        <div className="mb-2 flex flex-row">
+            <Image src="/time.png" alt="Icon" width={20} height={10} />
+            <p className='ml-2 text-sm'>{expireDate}</p>
+        </div>
       </div>
     </div>
   )
