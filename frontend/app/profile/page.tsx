9c8ab28b25dab23e38a156b51acee5a2f80f62c1
blackneb <antenehcs@gmail.com>
2023-11-05 22:01:13 +0300

    profile

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+'use client'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useRouter } from 'next/navigation'
 import Profile from '@/components/Profile'
 
 const page = () => {
+  const router = useRouter()
+  const isAuth = useSelector((state:any) => state.userAuth)
+  useEffect(() => {
+    if (!isAuth.isAuth){
+      router.push('/login')
+    }
+  }, [])
   return (
     <div>
         <Profile/>
